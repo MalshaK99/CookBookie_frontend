43a24761ab6f9a8968811af624f866cf99813be4
Malsha Karunarathna <cst20060@std.uwu.ac.lk>
2024-07-17 14:05:00 +0530

    login,signup

---
+++ CookBookie/src/Pages/signup.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import loginpage from '../Images/loginpage.jpg';
+import logoo from '../Images/logoo.png';
+
+const Signup = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+            <div className="flex w-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
+                {/* Left Side - Content */}
+                <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
+                    <div className="flex justify-center mx-auto">
+                        <img className="w-18 h-18 sm:w-20 sm:h-20" src={logoo} alt="logo" />
+                    </div>                  
+
+                    <div className="flex items-center justify-between mt-4">
+                        <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4"></span>
+
+                        <a href="#" className="text-xs text-center text-gray-500 uppercase dark:text-gray-400 hover:underline">WELCOME!!</a>
+
+                        <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
+                    </div>
+
+                    <form class="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
+                    <div>
+                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">First Name</label>
+                        <input type="text" placeholder="John" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+
+                    <div>
+                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Last name</label>
+                        <input type="text" placeholder="Snow" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+
+                    <div>
+                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Phone number</label>
+                        <input type="text" placeholder="XXX-XX-XXXX-XXX" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+
+                    <div>
+                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
+                        <input type="email" placeholder="johnsnow@example.com" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+
+                    <div>
+                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Password</label>
+                        <input type="password" placeholder="Enter your password" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+
+                    <div>
+                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Confirm password</label>
+                        <input type="password" placeholder="Enter your password" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+
+                    
+                </form>
+
+                    <div className="mt-6">
+                        <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+                            Sign Up
+                        </button>
+                    </div>
+
+                    <div className="flex items-center justify-between mt-4">
+                        <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+
+                        <a href="/login" className="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline">or sign In</a>
+
+                        <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+                    </div>
+                </div>
+
+                {/* Right Side - Image */}
+                <div className="hidden lg:block lg:w-1/2 relative">
+                    <img className="absolute inset-0 w-full h-full object-cover" src={loginpage} alt="loginpage" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Signup;
--- cookbookie/src/App.js
+++ cookbookie/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginP from './Pages/login';
+import Signup from './Pages/signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<LoginP />} />
+          <Route path='/signup' element={<Signup/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
